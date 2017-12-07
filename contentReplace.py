2c3d9d82c8a7542f41ed3bb64bfe3b6d5c0b6433
hein946 <Heinikov@gmail.com>
2017-12-07 18:32:30 +0900

    修正

--- contentReplace.py
+++ contentReplace.py
@@ -2,12 +2,14 @@ import os
 import re
 import codecs
 
-# ファイル一覧取得
-target_path = os.getcwd()
-files = os.listdir(target_path)
+# 置換処理設定
 reg_text = r'^\\'
 replace_text = '..\\\\'
 target_ext = '.m3u'
+
+# ファイル一覧取得
+target_path = os.getcwd()
+files = os.listdir(target_path)
 
 for file in files:
    if target_ext in file:
@@ -17,8 +19,8 @@ for file in files:
         for row in f:
             str = row.rstrip()
             str = str.replace('\ufeff','') # 不要な文字削除
-            new_str = re.sub(reg_text,replace_text,str) #正規表現置換
-            file_path.append(new_str)
+            str = re.sub(reg_text,replace_text,str) #正規表現置換
+            file_path.append(str)
 
         # 上書き書き込み
         with open(os.path.join(target_path ,file),'w',encoding = 'utf-8') as f:
