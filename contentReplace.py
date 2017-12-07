0c2287d5f3b398b69226a74865ec2a674f2e1889
hein946 <Heinikov@gmail.com>
2017-12-07 17:12:53 +0900

    first commit

---
+++ contentReplace.py
@@ -0,0 +1,28 @@
+import os
+import re
+import codecs
+
+# ファイル一覧取得
+target_path = os.getcwd()
+files = os.listdir(target_path)
+reg_text = r'^\\'
+replace_text = '..\\\\'
+target_ext = '.m3u'
+
+for file in files:
+   if target_ext in file:
+        f = codecs.open(os.path.join(target_path ,file), 'r','utf-8')
+        file_path = []
+        #指定ファイル内容置換処理
+        for row in f:
+            str = row.rstrip()
+            str = str.replace('\ufeff','') # 不要な文字削除
+            new_str = re.sub(reg_text,replace_text,str) #正規表現置換
+            file_path.append(new_str)
+
+        # 上書き書き込み
+        with open(os.path.join(target_path ,file),'w',encoding = 'utf-8') as f:
+            f.write("\n".join(file_path))
+        print("修正完了:{}".format(file))
+        f.close
+
